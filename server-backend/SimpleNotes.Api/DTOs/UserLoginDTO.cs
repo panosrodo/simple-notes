6fc97ae317cac1227ee4dba8f59a08e1dadebc6d
panosrodo <brandonroots@hotmail.gr>
2026-01-08 21:58:35 +0200

    feat: add user login DTO

---
+++ server-backend/SimpleNotes.Api/DTOs/UserLoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleNotes.Api.DTOs
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, one digit, and one special character")]
+        public string? Password { get; set; }
+    }
+}
