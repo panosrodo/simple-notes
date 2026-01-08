adff143b56ff5457370ce266a9994ecc25924b88
panosrodo <brandonroots@hotmail.gr>
2026-01-08 22:01:57 +0200

    feat: add user read-only DTO

---
+++ server-backend/SimpleNotes.Api/DTOs/UserReadOnlyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleNotes.Api.DTOs
+{
+    public class UserReadOnlyDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string UserRole { get; set; } = null!;
+    }
+}
