ab7d08392e888572e4d13bff534a4c9f17c85fa9
panosrodo <brandonroots@hotmail.gr>
2026-01-08 22:03:32 +0200

    feat: add DTOs for note management

---
+++ server-backend/SimpleNotes.Api/DTOs/NoteDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleNotes.Api.DTOs
+{
+    public class NoteDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string? Tags { get; set; }
+        public bool IsPinned { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ server-backend/SimpleNotes.Api/DTOs/UpdateNoteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleNotes.Api.DTOs
+{
+    public class UpdateNoteDTO
+    {
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; } = null!;
+
+        public string? Tags { get; set; }
+        public bool IsPinned { get; set; }
+    }
+}
