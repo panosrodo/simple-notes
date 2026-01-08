0169a9dff42ac4ff5c85cf881cdf4d4d8208dc07
panosrodo <brandonroots@hotmail.gr>
2026-01-08 21:59:00 +0200

    feat: add user signup DTO

---
+++ server-backend/SimpleNotes.Api/DTOs/UserSignupDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleNotes.Api.DTOs
+{
+    public class UserSignupDTO
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(100, ErrorMessage = "Email must not exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, one digit, and one special character")]
+        public string? Password { get; set; }
+    }
+}
