b783e41ef05dbbb9ccda7868650032022f310557
panosrodo <brandonroots@hotmail.gr>
2026-01-12 21:21:41 +0200

    feat: add /users/me endpoint

---
+++ server-backend/SimpleNotes.Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleNotes.Api.Services.Interfaces;
+
+namespace SimpleNotes.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _users;
+
+    public UsersController(IUserService users)
+    {
+        _users = users;
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> Me()
+    {
+        var me = await _users.GetMeAsync();
+        return Ok(me);
+    }
+}
